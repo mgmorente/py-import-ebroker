8eee985cf693c2847003d63d80ae36300bf8803b
morente <mgmorente@gmail.com>
2022-11-27 12:45:21 +0100

    First commit

---
+++ importarClientes.py
@@ -0,0 +1,277 @@
+import json
+import psycopg2
+from config import config
+from utils import *
+
+# Fn borrar datos y tablas
+def borrar_datos():
+    cur = conn.cursor()
+    cur.execute("DELETE FROM clientes WHERE tag = 'INI'")
+    cur.execute("DELETE FROM polizas WHERE tag = 'INI'")
+    cur.execute("DELETE FROM relacion_codigo_cliente_nif")
+    conn.commit()
+    cur.close()
+
+# Fn ver datos por pantalla
+def ver_datos(r):
+    print(
+        r["cod_cliente"],
+        r["cif_nif_cliente"],
+        # r["cliente_fis_jur"],
+        # r["cod_escivil"],
+        # r["cliente_fis_jur"],
+        # valida_fecha(r["fecha_alta_cliente"]),
+        r["nombre_cliente"],
+        # r["dir_cliente"],
+        # r["cp_cliente"],
+        # r["poblacion_cliente"],
+        # r["cod_provincia"],
+        # r["cod_sexo"],
+        # valida_fecha(r["fecha_baja_cliente"]),
+        # valida_fecha(r["fecha_naci_cliente"]),
+        r["telefono"],
+        sep='; ', end='\n')
+
+# Fn insertar datos en lista
+def insertar_cliente_lista(r):
+    clientesPaccList.append((
+        valida_nif(r["cif_nif_cliente"], r["cod_cliente"]), 
+        valida_cadena(r["nombre_cliente"],150),
+        valida_cadena(r["nombre"],45), 
+        valida_cadena(r["apellido1"],45),
+        valida_cadena(r["apellido2"],45),
+        valida_cadena(r["dir_cliente"],45),
+        valida_cadena(r["cp_cliente"],5),
+        valida_cadena(r["poblacion_cliente"],30),
+        valida_entero(r["cod_provincia"]),
+        valida_telefono(r["telefono"]),         #telefono
+        valida_telefono(r["telefono"], True),   #movil
+        valida_fecha(r["fecha_naci_cliente"]),  
+        valida_fecha(r["fecha_carnet"]), 
+        valida_persona(r["cliente_fis_jur"]),   #persona fisica juridica
+        valida_ecivil(r["cod_escivil"]),        #estado civil       
+        'now()',                                #fecha alta 
+        sucursal,                               #sucursal
+        colaborador,                            #colaborador
+        'now()',                                #create_at
+        tag,                                    #tag
+        ''                                      #passweb
+        ))
+
+# Fn insertar datos en lista
+def insertar_cliente_codnif_lista(r):
+    if r["cif_nif_cliente"] is None:
+        return
+    clientesCodNifList.append((
+        r["cod_cliente"],
+        r["cif_nif_cliente"], 
+        ))
+
+# Fn recuperar nif a partir de codigo cliente
+def getNif(cod_cliente):
+    cod_cliente = int(cod_cliente)
+    try:
+        return clientesCodNifListDict[cod_cliente]
+    except KeyError:
+        # prRed(f"ERR: NO EXISTE EL COD_CLIENTE {cod_cliente}")
+        return ''
+
+# Fn equivalencia ramos pacc
+def getRamoPacc(a,b,c):
+    return '614'
+
+# Fn ver datos por pantalla
+def ver_datos_polizas(r):
+    print(
+        r["cod_poliza_cia"],                                # cia poliza
+        getCodCia(r["cia_poliza"]),                         # compania
+        getRamoPacc(r["producto"],r["ramo"],r["descripcion_pro"]),   # producto
+        valida_fecha(r["fecha_efecto"]),                    # fecha efecto    
+        valida_fecha(r["pro_vto_rec"]),                     # fecha vencimiento
+        getSituacion(r["estado"]),                          # situacion
+        getNif(r["n_cliente"]),                             # nif
+        '',                                                 # nif asegurado
+        False,                                              # ase_es_asegurado
+        r['matricula'],                                     # matricula
+        r["forma_pago"][0:3],                               # forma pago
+        tipo_poliza,                                        # tipo poliza
+        'x',                                                # objeto
+        'xxx',                                              # comentario
+        'now()',                                            # fecha alta
+        valida_fecha(r["fecha_anulacion"]),                 # fecha anulacion
+        valida_fecha(r["fecha_anulacion"]),                 # fecha anulacion sis
+        getMotivoAnulacion(r["estado"]),                    # motivo anulacion
+        canal,                                              # canal
+        '-vacio-',                                          # iban
+        sucursal,                                           # sucursal
+        colaborador,                                        # colaborador
+        tag,                                                # tag
+        sep='; ', end='\n\n')
+
+# Fn capturar contador y añadir ceros a la izquierda
+def getNewContrato():
+    contrato = ''
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM incrementa_contador('SOLICITUDPOL')")
+        contrato = sucursal + str(cur.fetchone()[0]).zfill(8)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    return contrato
+
+# Fn insertar datos en lista
+def insertar_poliza_lista(r):
+    polizasPaccList.append((
+        getNewContrato(),
+        r["cod_poliza_cia"],                                # cia poliza
+        getCodCia(r["cia_poliza"]),                         # compania
+        getRamoPacc(r["producto"],r["ramo"],r["descripcion_pro"]),   # producto
+        valida_fecha(r["fecha_efecto"]),                    # fecha efecto    
+        valida_fecha(r["pro_vto_rec"]),                     # fecha vencimiento
+        getSituacion(r["estado"]),                          # situacion
+        getNif(r["n_cliente"]),                             # nif
+        '',                                                 # nif asegurado
+        False,                                              # ase_es_asegurado
+        valida_cadena(r['matricula'],15),                                     # matricula
+        r["forma_pago"][0:3],                               # forma pago
+        tipo_poliza,                                        # tipo poliza
+        'x',                                                # objeto
+        'xxx',                                              # comentario
+        'now()',                                            # fecha alta
+        valida_fecha(r["fecha_anulacion"]),                 # fecha anulacion
+        valida_fecha(r["fecha_anulacion"]),                 # fecha anulacion sis
+        getMotivoAnulacion(r["estado"]),                    # motivo anulacion
+        canal,                                              # canal
+        '-vacio-',                                          # iban
+        sucursal,                                           # sucursal
+        colaborador,                                        # colaborador
+        tag, 
+        ))
+
+# Fn insertar cliente
+def insertar_clientes_bd():
+    sql = """INSERT INTO clientes(nif,nombre,cli_nombre,cli_apellido1,cli_apellido2,domicilio,cpostal,poblacion,provincia,tel_privado,movil,fecha_nacimiento,fecha_carnet,persona,ecivil,fecha_alta,sucursal,colaborador,created_at,tag,passweb)
+             VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                ON CONFLICT ON CONSTRAINT clientes_pk 
+                DO NOTHING"""
+    
+    vendor_id = None
+    try:
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.executemany(sql, clientesPaccList)
+        cur.executemany("INSERT INTO relacion_codigo_cliente_nif VALUES(%s,%s)", clientesCodNifList)
+        cur.execute("UPDATE clientes SET nombre2 = nombre, domicilio2 = domicilio, poblacion2 = poblacion, cpostal2 = cpostal, provincia2 = provincia WHERE tag = 'INI'")
+        # get the generated id back
+        # vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    return vendor_id
+
+# Fn insertar cliente
+def insertar_polizas_bd():
+    sql = """INSERT INTO polizas 
+            (poliza,cia_poliza,compania,producto,fecha_efecto,fecha_vencimiento,situacion,nif,nif_asegurado,ase_es_asegurado,matricula,forma_pago,
+            tipo_poliza,objeto,comentario,fecha_alta,fecha_anula,fecha_anula_sis,causa_anula,canal,iban,sucursal,colaborador,tag) 
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    try:
+        cur = conn.cursor()
+        cur.executemany(sql, polizasPaccList)
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    return 
+   
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# Variables
+sucursal = '2901'
+colaborador = '29010001'
+tag = 'INI'
+tipo_poliza = 1
+canal = 6
+path_files = 'datos/'
+path_files = 'CTM/BBDD/'
+
+# read database configuration
+params = config()
+# connect to the PostgreSQL database
+conn = psycopg2.connect(**params)
+### temporal ###
+borrar_datos()
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#  CLIENTES
+
+# Lectura fichero con multiples JSON
+clientesList = []
+with open(path_files + 'eb_clientes.txt', encoding="utf8") as f:
+    for jsonObj in f:
+        resourceDict = json.loads(jsonObj)
+        clientesList.append(resourceDict)
+
+# Ver nombres columnas
+# print('\n'.join(list(resourceDict.keys())))
+
+# Bucle clientes
+clientesPaccList = []
+clientesCodNifList = []
+for r in clientesList:
+    ver_datos(r)
+    insertar_cliente_lista(r)
+    insertar_cliente_codnif_lista(r)
+
+# Ver datos
+# print('\n',clientesPaccList)
+
+clientesCodNifListDict = dict(clientesCodNifList)
+quit()
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#  POLIZAS
+
+# Lectura fichero con multiples JSON
+polizasList = []
+with open(path_files + 'eb_polizas.txt', encoding="utf8") as f:
+    for jsonObj in f:
+        resourceDict = json.loads(jsonObj)
+        polizasList.append(resourceDict)
+
+# Ver nombres columnas
+# print('\n'.join(list(resourceDict.keys())))
+
+# Bucle polizas
+polizasPaccList = []
+for r in polizasList:
+    # ver_datos_polizas(r)
+    insertar_poliza_lista(r)
+
+# Ver datos
+# print('\n',polizasPaccList)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#  PROCESAR
+
+# Insertar bd
+insertar_clientes_bd()
+insertar_polizas_bd()
+# Cerrar conexion
+if conn is not None:
+    conn.close()
+
+# Mensaje fin proceso
+prGreen('Success!')
---
+++ utils.py
@@ -0,0 +1,166 @@
+from datetime import datetime
+
+# Fn formatear fecha o devolver null
+def valida_fecha(fecha = ''):
+    try:
+        f = datetime.strptime(fecha["$date"],"%Y-%m-%dT%H:%M:%S.000Z")
+        return f.strftime("%Y-%m-%d")
+    except:
+        return None
+
+# Fn validar texto y longitud
+def valida_cadena(texto, longitud):
+    if texto is None:
+        return ''
+    else:     
+        return texto[0:longitud]
+
+def valida_entero(numero):
+    if numero is None:
+        return 0
+    else:
+        return numero
+
+def valida_nif(nif, codigo):
+    if nif is None:
+        return str(codigo).zfill(9)
+    else:
+        return nif
+
+# Fn validar telefono o movil
+def valida_telefono(numero, campo_movil = False):
+    if numero is None:
+        return ''
+    
+    numero = only_numerics(numero)
+    if not numero:
+        return ''
+    
+    es_numero_movil = True if int(numero[0:1]) in [6,7] else False
+    if es_numero_movil and campo_movil:
+        return numero
+    elif not es_numero_movil and not campo_movil:
+        return numero
+    else:
+        return '' 
+
+def valida_persona(valor):
+    if valor is None:
+        return ''
+    
+    if valor == 'F':
+        return 'FIS'
+    elif valor == 'J':
+        return 'JUR'
+    else:
+        return ''
+
+def valida_ecivil(valor):
+    if valor is None:
+        return ''
+    
+    if valor == 'C':
+        return 'CAS'
+    elif valor == 'V':
+        return 'VID'
+    elif valor == 'D':
+        return 'DIV'
+    elif valor == 'S':
+        return 'SOL' 
+    elif valor == 'S':
+        return 'SEP'
+    else:
+        return ''    
+
+def getSituacion(valor):
+    if valor is None:
+        return 0
+    if valor == "ANULADA":
+        return 5
+    else:
+        return 1
+
+def getMotivoAnulacion(valor):
+    if valor == "ANULADA":
+        return 7
+    else:
+        return 0    
+
+def getCodCia(codigo):
+    if codigo is None:
+        return 0
+        
+    codigo = int(codigo)
+
+    if codigo == 19: 
+        return 22; #"nombre_cia" : "ASISA, ASISTENCIA SANITARIA INTERPROVINCIAL DE SEGUROS, S.A." ,
+    elif codigo == 24: 
+        return 7; # "SANTA LUCIA, S.A. COMPAÑIA DE SEGUROS Y REASEGUROS" , "cia_abre
+    elif codigo == 25: 
+        return 198; # "AXA PENSIONES S.A ENT.GEST.DE FONDOS" , "cia_abreviatura" : "AX
+    elif codigo == 12: 
+        return 14; # "OCASO, S.A., COMPAÑIA DE SEGUROS Y REASEGUROS." , "cia_abreviat
+    elif codigo == 20: 
+        return 0; # "MUTUAL DE CONDUCTORS, MUTUALIDAD DE PREVISION SOCIAL A PRIMA FI
+    elif codigo == 21: 
+        return 0; # "SVRNE, MUTUA DE SEGUROS Y REASEGUROS A PRIMA FIJA" , "cia_abrev
+    elif codigo == 18: 
+        return 42; # "SANTA LUCIA VIDA Y PENSIONES S.A" , "cia_abreviatura" : "SANTA 
+    elif codigo == 8 : 
+        return 1; #"PLUS ULTRA SEGUROS GENERALES Y VIDA, S.A. DE SEGUROS Y REASEGURO
+    elif codigo == 2 : 
+        return 33; #"ARAG SE SUC.ESPAÐA" , "cia_abreviatura" : "ARAG" , "cod_interno_
+    elif codigo == 1 : 
+        return 13; #"ALLIANZ, COMPAÑIA DE SEGUROS Y REASEGUROS, SOCIEDAD ANONIMA" , "
+    elif codigo == 22: 
+        return 0; # "W.R. BERKLEY EUROPE AG SUC. ESPAÑA" , "cia_abreviatura" : "BERK
+    elif codigo == 16: 
+        return 0; # "L'EQUITE - XENASEGUR " , "cia_abreviatura" : "XENASEGUR" , "cod
+    elif codigo == 26: 
+        return 58; # "MAPFRE VIDA, S.A. DE SEGUROS Y REASEGUROS SOBRE LA VIDA HUMANA"
+    elif codigo == 14: 
+        return 26; # "SANITAS, SOCIEDAD ANONIMA DE SEGUROS." , "cia_abreviatura" : "S
+    elif codigo == 27: 
+        return 0; # "W.R. BERKLEY EUROPE AG SUC. ESPAÑA" , "cia_abreviatura" : "BERK
+    elif codigo == 5 : 
+        return 4; #"DKV SEGUROS Y REASEGUROS, SOCIEDAD ANONIMA ESPAÑOLA" , "cia_abre
+    elif codigo == 11: 
+        return 38; # "REALE SEGUROS GENERALES, S.A." , "cia_abreviatura" : "REALE" , 
+    elif codigo == 17: 
+        return 53; # "AEGON ESPAÐA, SOCIEDAD ANËNIMA DE SEGUROS Y REASEGUROS" , "cia_
+    elif codigo == 23: 
+        return 21; # "AXA AURORA VIDA, S.A. DE SEGUROS Y REASEGUROS" , "cia_abreviatu
+    elif codigo == 13: 
+        return 9; # "ZURICH INSURANCE PLC SUC.ESPAÑA" , "cia_abreviatura" : "ZURICH"
+    elif codigo == 4 : 
+        return 2; #"AXA SEGUROS GENERALES, S. A. DE SEGUROS Y REASEGUROS" , "cia_abr
+    elif codigo == 9 : 
+        return 34; #"LIBERTY SEGUROS, COMPAÑIA DE SEGUROS Y REASEGUROS, S.A." , "cia_
+    elif codigo == 7 : 
+        return 8; #"GENERALI ESPAÑA, SOCIEDAD ANÓNIMA DE SEGUROS Y REASEGUROS" , "ci
+    elif codigo == 10: 
+        return 27; # "MAPFRE FAMILIAR, COMPAÑIA DE SEGUROS Y REASEGUROS, S.A." , "cia
+    elif codigo == 6 : 
+        return 49; #"FIATC, MUTUA DE SEGUROS Y REASEGUROS A PRIMA FIJA" , "cia_abrevi
+    elif codigo == 3 : 
+        return 46; #"ASEFA, S.A. COMPAÑÍA ESPAÑOLA DE SEGUROS Y REASEGUROS" , "cia_ab
+       
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+ 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+ 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+ 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+ 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+ 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+ 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+ 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
+def only_numerics(seq):
+    seq_type= type(seq)
+    return seq_type().join(filter(seq_type.isdigit, seq))
