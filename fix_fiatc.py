ea2ca96cb7de6ec228d9eabd6c7d5a4a40d12609
mgmorente <mgmorente@gmail.com>
2023-01-11 12:33:10 +0100

    Arreglo polizas fiatc

--- fix_fiatc.py
+++ fix_fiatc.py
@@ -1,17 +1,25 @@
 import xml.etree.ElementTree as ET
 import psycopg2
 
+from config import config
+from utils import prCyan, prRed
+
 ######### bd
-conn = psycopg2.connect(database="ctm",
-                        host="localhost",
-                        user="postgres",
-                        password="postgres",
-                        port="5432")
+# conn = psycopg2.connect(database="ctm",
+#                         host="localhost",
+#                         user="postgres",
+#                         password="postgres",
+#                         port="5432")
+
+# read database configuration
+params = config()
+# connect to the PostgreSQL database
+conn = psycopg2.connect(**params)
 
 polizasBdList = []
 
 cur = conn.cursor()
-cur.execute("select cia_poliza, nif from polizas where compania = 49")
+cur.execute("select cia_poliza, nif, poliza from polizas where compania = 49")
 
 for table in cur.fetchall(): 
     polizasBdList.append(table)
@@ -24,7 +32,7 @@ cur.close()
 tree = ET.parse('fiatc-cartera.xml')
 root = tree.getroot()
 
-xmlList = []
+# xmlList = []
 contador = 0
 
 for poliza in root.findall("Objetos/Poliza"):
@@ -36,10 +44,19 @@ for poliza in root.findall("Objetos/Poliza"):
     if nif is None:
         nif = poliza.find('Tomador/PersonaJuridica/IdPersona')
     
-    xmlList.append( (idPoliza.text, ramo.text, nif.text) )
+    for poliza in polizasBdList:
+        # comparar nif
+        if poliza[1] == nif.text:
+            # comparar 2 primeros digitos y ultimos 4 digitos de la poliza
+            if poliza[0][-4:] == idPoliza.text[-4:] and poliza[0][0:2] == idPoliza.text[0:2]:
+                nRamo = ramo.text.ljust(2,'0')
+                nPoliza = str(idPoliza.text).zfill(7)
+                # print(poliza[0],  nRamo + nPoliza)
+                print(f"UPDATE polizas SET cia_poliza = '{nRamo + nPoliza}' WHERE poliza = '{poliza[2]}';")
+                contador += 1
+    
+    # xmlList.append( (idPoliza.text, ramo.text, nif.text) )
 
-    contador += 1
-
-print(' -- Numero registros', contador, '--')
+prCyan(f'>> {contador} registros')
 
 
